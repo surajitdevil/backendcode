efea354a107c7782959d981bd0dc141d3dcbe897
surajitdevil <surajitsarmacharjeemphasis@gmail.com>
2026-04-05 03:54:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
-const fetch = require("node-fetch");
+
 require("dotenv").config();
 
 const app = express();
